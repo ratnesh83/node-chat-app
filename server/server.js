3671abe698bfcaa874a1044e8b3959ecabe4eb56
Ratnesh Singh <ratnesh_singh@outlook.com>
2018-07-01 09:25:21 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message')
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
@@ -15,27 +16,14 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('new user connected');
 
-    socket.emit('newMessage',{
-        from:'Admin',
-        text:'Welcome to chat app',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
 
-    socket.broadcast.emit('newMessage',{
-        from:'Admin',
-        text: 'New User Joined',
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'));
 
- 
-
-    socket.on('createMessage',(message)=>{
+    socket.on('createMessage',(message,callback)=>{
         console.log('createMessage',message);
-        io.emit('newMessage',{
-            from:message.from,
-            text:message.text,
-            createdAt:new Date().getTime()
-        });
+        io.emit('newMessage',generateMessage(message.from,message.text));
+        callback('server');
         // socket.broadcast.emit('newMessage',{
         //     from:message.from,
         //     text:message.text,
