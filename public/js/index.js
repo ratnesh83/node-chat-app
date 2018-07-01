9b499a915714324336444132ffa1c85c4afe90c8
Ratnesh Singh <ratnesh_singh@outlook.com>
2018-07-01 14:08:53 +0530

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -8,21 +8,41 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage',function(message){
-    console.log('New Message',message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template,{
+        text:message.text,
+        from:message.from,
+        createdAt:formattedTime
+    });
 
-    jQuery('#messages').append(li)
+    jQuery('#messages').append(html);
+    // var formattedTime = moment(message.createdAt).format('h:mm a');
+    // console.log('New Message',message);
+    // var li = jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+    // jQuery('#messages').append(li)
 });
 
 socket.on('newLocationMessage',function(message){
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template,{
+        from:message.from,
+        url:message.url,
+        createdAt:formattedTime
+    });
 
-    li.text(`${message.from}`);
-    a.attr('href',message.url);
-    li.append(a);
-    jQuery('#messages').append(li)
+    jQuery('#messages').append(html);
+
+    // var li = jQuery('<li></li>');
+    // var a = jQuery('<a target="_blank">My current location</a>');
+
+    // li.text(`${message.from} ${formattedTime}:`);
+    // a.attr('href',message.url);
+    // li.append(a);
+    // jQuery('#messages').append(li)
 
 });
 
@@ -45,14 +65,16 @@ locationButton.on('click',function(){
         return alert('Geolocation not supported by your location');
     }
 
-    locationButton.attr('disabled','disabled');
+    locationButton.attr('disabled','disabled').text('Sending Location...');
     
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     },function(){
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location');
     });
 });
