dcb3fe14d85e274157243e1b954a315e0f7227f2
Ratnesh Singh <ratnesh_singh@outlook.com>
2018-07-01 14:22:52 +0530

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 var socket = io();
+
+function scrollToBottom(){
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
 socket.on('connect', function () {
     console.log('connected to server');
 });
@@ -16,7 +30,9 @@ socket.on('newMessage',function(message){
         createdAt:formattedTime
     });
 
+
     jQuery('#messages').append(html);
+    scrollToBottom();
     // var formattedTime = moment(message.createdAt).format('h:mm a');
     // console.log('New Message',message);
     // var li = jQuery('<li></li>');
@@ -35,6 +51,8 @@ socket.on('newLocationMessage',function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
+
 
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My current location</a>');
